2703d6fa002543ad3f2f83d087f0724e3fbfc305
NongComEng <anuchit.prasertsang@gmail.com>
2014-10-27 23:59:32 +0700

    Test find subject by Course Pass

--- src/test/java/com/spt/evt/dao/SubjectDaoTest.java
+++ src/test/java/com/spt/evt/dao/SubjectDaoTest.java
@@ -2,6 +2,8 @@ package com.spt.evt.dao;
 
 import static org.hamcrest.core.Is.is;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.spt.evt.entity.Base;
 import com.spt.evt.entity.Course;
+import com.spt.evt.entity.Subject;
 
 /**
  * Created by : Anuchit Prasertsang Created Date : 27/10/2014
@@ -20,12 +23,23 @@ public class SubjectDaoTest extends AbstractTestDao {
 	@Autowired
 	private SubjectDao subjectDao;
 
+	@Autowired
+	private CourseDao courseDao;
+
 	public SubjectDao getSubjectDao() {
 		return subjectDao;
 	}
 
 	public void setSubjectDao(SubjectDao subjectDao) {
 		this.subjectDao = subjectDao;
+	}
+
+	public CourseDao getCourseDao() {
+		return courseDao;
+	}
+
+	public void setCourseDao(CourseDao courseDao) {
+		this.courseDao = courseDao;
 	}
 
 	@Test
@@ -35,7 +49,10 @@ public class SubjectDaoTest extends AbstractTestDao {
 
 	@Test
 	public void testFindSubjectByCourseShouldBeNotNull() {
-		
+		Course course = new Course();
+		course.setId(1L);
+		List<Subject> subjects = this.getSubjectDao().findByCourse(course);
+		Assert.assertNotNull(subjects);
 	}
 
 }
