1a620cf771b4626bbfdd95c1f84655ea29b93dcc
NongComEng <anuchit.prasertsang@gmail.com>
2014-11-04 08:03:08 +0700

    Test Pass All Service

--- src/test/java/com/spt/evt/controller/AbstractTestController.java
+++ src/test/java/com/spt/evt/controller/AbstractTestController.java
@@ -20,7 +20,6 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@Transactional
 @ActiveProfiles("test")
 @Configurable
 @ContextConfiguration({"classpath:META-INF/spring/evaluatetool-context.xml", "classpath:META-INF/spring/evaluatetool/evaluatetool-servlet.xml"})
