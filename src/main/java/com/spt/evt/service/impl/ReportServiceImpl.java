5829d35470b4ac1e917311dabcce000d027b4b38
Anupat Ruangsuwan <l3igl3all@hotmail.com>
2014-12-17 13:06:24 +0700

    Edit Report and Fix RoomDaoImpl

--- src/main/java/com/spt/evt/service/impl/ReportServiceImpl.java
+++ src/main/java/com/spt/evt/service/impl/ReportServiceImpl.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 import com.spt.evt.entity.*;
 
-import org.json.JSONObject;
+import org.json.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -20,8 +20,58 @@ public class ReportServiceImpl extends ProviderService implements ReportService{
 	@Override
 	public JSONObject getAllScore() {
 		String roomStatus = "Completed";
+		List<Room> rooms = this.getRoomService().findByStatus(roomStatus);						
+		JSONObject reportNoSort = generateScoreJsonObjectByRoom(rooms);
+		JSONArray reportNoSortArray = new JSONArray(""+reportNoSort.get("report")) ;
+			
+		TreeMap<String,List<JSONObject>> reportTreeMap = new TreeMap<String,List<JSONObject>>();
+		for(int n = 0; n < reportNoSortArray.length(); n++)
+			{
+				if (reportTreeMap.containsKey(reportNoSortArray.getJSONObject(n).getString("examiner"))) {
+					List<JSONObject> listObject = reportTreeMap.get(reportNoSortArray.getJSONObject(n).getString("examiner"));
+					listObject.add(reportNoSortArray.getJSONObject(n));
+					reportTreeMap.put(reportNoSortArray.getJSONObject(n).getString("examiner"),listObject);
+
+				}
+				else{
+					List<JSONObject> listObject = new ArrayList<JSONObject>();
+					listObject.add(reportNoSortArray.getJSONObject(n));
+					reportTreeMap.put(reportNoSortArray.getJSONObject(n).getString("examiner"),listObject);
+				}
+			}
+			
+		JSONArray reportArray = new JSONArray() ;
+		JSONObject report = new JSONObject();
+		for(String key: reportTreeMap.keySet()) {
+			for (JSONObject jsList : reportTreeMap.get(key)) {	
+				reportArray.put(jsList);
+			}
+		}
+		report.put("report",reportArray);	
+		return report;
+	}
+
+	@Override
+	public JSONObject getAllExaminerCompleted() {
+		JSONObject report = new JSONObject();
+		String roomStatus = "Completed";
 		List<Room> rooms = this.getRoomService().findByStatus(roomStatus);
-		JSONObject report = generateScoreJsonObjectByRoom(rooms);
+		Map map = new HashMap();
+
+		for(Room c:rooms){
+			if(map.get(c.getName())==null){
+				map.put(c.getName(),c);
+			}
+		}	
+			
+		List<Room> roomsList = new ArrayList<Room>(map.values());
+		for (Room room : roomsList ) {
+			JSONObject examinerReport = new JSONObject();
+			Person examiner = this.getParticipantsService().findByExaminerInRoom(room);
+			examinerReport.put("examinerId",examiner.getId());
+			examinerReport.put("examiner", examiner.getName()+" "+examiner.getLastName());
+			report.append("report", examinerReport);
+		}
 		return report;
 	}
 
@@ -42,9 +92,11 @@ public class ReportServiceImpl extends ProviderService implements ReportService{
 			examinerReport.put("examinerId",examiner.getId());
 			examinerReport.put("examiner", examiner.getName()+" "+examiner.getLastName());
 			DateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-			examinerReport.put("dateTest", formatDate.format(keyScoreCalculated.getStartTime()));
-
+			DateFormat formatDate2 = new SimpleDateFormat("HH:mm");
+			examinerReport.put("dateTest", formatDate.format(keyScoreCalculated.getStartTime())+" - "
+				+formatDate2.format(keyScoreCalculated.getEndTime()));
 			report.append("report", examinerReport);
+			examinerReport.put("nameRoom",keyScoreCalculated.getName());
 		}
 		return report;
 	}
@@ -86,7 +138,10 @@ public class ReportServiceImpl extends ProviderService implements ReportService{
 		List<Participants> participantsList = this.getParticipantsService().findByPerson(examiner);
 		List<Room> rooms = new ArrayList<Room>();
 		for(Participants participants:participantsList){
-			rooms.add(participants.getRoom());
+			Room room = participants.getRoom();
+			if (room.getStatus().equals("Completed")) {
+				rooms.add(participants.getRoom());	
+			}
 		}
 		JSONObject report = generateScoreJsonObjectByRoom(rooms);
 		return report;
